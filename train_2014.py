52df00c18743d25124ac1f6f347a23a16f0dbbbd
DMaier <edye@Edyes-Air.home>
2016-08-03 23:53:32 -0400

    Read in feature columns and labels

--- train_2014.py
+++ train_2014.py
@@ -1,7 +1,13 @@
-from __future__ import print_function
+# from __future__ import print_function
 import pandas as pd
 
 inputFilePath = '/Users/edye/dev/birth_records/2014_births.txt'
 
-d = pd.read_csv(inputFilePath,delimiter="\t", nrows=1000 )
-print(d.numWeeksAtBirthSinceLastPeriod) 
+d = pd.read_csv(inputFilePath,delimiter="\t", nrows=100000 )
+
+# Extract feature (X) and target (y) columns
+feature_cols = list(d.columns[2:])  # all columns but first two are features
+target_col = d.columns[0]  # first column is the target/label
+print "Feature column(s):-\n{}".format(feature_cols)
+print "Target column: {}".format(target_col)
+print "Total number of births: {}".format(len(d.index))
